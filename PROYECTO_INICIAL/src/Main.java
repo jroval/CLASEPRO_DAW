22305c686243126e1c7c24977f4de19ceee98d73
J.Robles <jaimit1988@gmail.com>
2025-10-02 19:56:45 +0200

    update

---
+++ PROYECTO_INICIAL/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Este es la segunda linea del programa");
+        System.out.println("Este es la segunda linea del programa");
+    }
+
+    public void nombreMetodo() {
+    }
+
+
+}
+
+
+
